f35b9566afabffaa309fb48f455ccb6a0ae7666e
zombieBOY <zoubairgaming5@gmail.com>
2024-05-10 20:11:33 +0100

    d

---
+++ C00/ex07/ft_putnbr.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		write(1, "-", 1);
+		write(1, "2", 1);
+		nb = 147483648;
+	}
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if (nb < 10)
+	{
+		ft_putchar(nb + '0');
+	}
+}
+/*int main()
+{
+	ft_putnbr(-28);
+	return 0;
+}*/
