2b9e330de5cdc3a99575dda8706f636cda55673e
zombieBOY <zoubairgaming5@gmail.com>
2024-06-12 01:44:09 +0100

    sf

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -6,37 +6,40 @@
 /*   By: zm <marvin@42.fr>                          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:08:54 by zm                #+#    #+#             */
-/*   Updated: 2024/06/06 18:52:23 by zm               ###   ########.fr       */
+/*   Updated: 2024/06/12 01:43:49 by zm               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <stdio.h>
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	int	ret;
+	unsigned int	z;
 	unsigned int	i;
 
 	i = 0;
-	ret = 0;
+	z = 0;
 	while ((s1[i] != '\0' || s2[i] != '\0') && i < n)
 	{
 		if (s1[i] == s2[i])
 		{
-			ret = 0;
+			z = 0;
+		}
+		else if ((s1[i] - s2[i]) < 0)
+		{
+			return (-1);
 		}
 		else
 		{
-			ret = s1[i] - s2[i];
-			return (ret);
+			return (1);
 		}
 		i++;
 	}
-	return (ret);
+	return (z);
 }
 /*int main ()
 {
-	char s1[] = "";
-	char s2[] = "123456789";
+	char s1[] = "9123";
+	char s2[] = "55555555555123";
 	printf("  %d", ft_strncmp(s1,s2,7));
 	return 0;
 }*/
